8af50c6782e45e911f69bf9b4927f0dcf1ffb278
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-03-31 17:56:56 -0300

    Clean index.js default componentes

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -8,23 +8,13 @@ const GridBlock = CompLibrary.GridBlock;
 
 class HomeSplash extends React.Component {
   render() {
-    const {siteConfig, language = ''} = this.props;
-    const {baseUrl, docsUrl} = siteConfig;
-    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
-    const langPart = `${language ? `${language}/` : ''}`;
-    const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
+    const { siteConfig } = this.props;
 
     const SplashContainer = props => (
       <div className="homeContainer">
         <div className="homeSplashFade">
           <div className="wrapper homeWrapper">{props.children}</div>
         </div>
-      </div>
-    );
-
-    const Logo = props => (
-      <div className="projectLogo">
-        {/* <img src={props.img_src} alt="Project Logo" /> */}
       </div>
     );
 
@@ -35,32 +25,10 @@ class HomeSplash extends React.Component {
       </h2>
     );
 
-    const PromoSection = props => (
-      <div className="section promoSection">
-        <div className="promoRow">
-          <div className="pluginRowBlock">{props.children}</div>
-        </div>
-      </div>
-    );
-
-    const Button = props => (
-      <div className="pluginWrapper buttonWrapper">
-        <a className="button" href={props.href} target={props.target}>
-          {props.children}
-        </a>
-      </div>
-    );
-
     return (
       <SplashContainer>
-        {/* <Logo img_src={`${baseUrl}img/docusaurus.svg`} /> */}
         <div className="inner">
           <ProjectTitle siteConfig={siteConfig} />
-          {/* <PromoSection>
-            <Button href="#try">Try It Out</Button>
-            <Button href={docUrl('doc1.html')}>Example Link</Button>
-            <Button href={docUrl('doc2.html')}>Example Link 2</Button>
-          </PromoSection> */}
         </div>
       </SplashContainer>
     );
@@ -70,7 +38,6 @@ class HomeSplash extends React.Component {
 class Index extends React.Component {
   render() {
     const {config: siteConfig, language = ''} = this.props;
-    const {baseUrl} = siteConfig;
 
     const Block = props => (
       <Container
@@ -93,33 +60,6 @@ class Index extends React.Component {
         <MarkdownBlock>These are features of this project</MarkdownBlock>
       </div>
     ); 
-
-    /* const TryOut = () => (
-      <Block id="try">
-        {[
-          {
-            content: 'Talk about trying this out',
-            //image: `${baseUrl}img/docusaurus.svg`,
-            //imageAlign: 'left',
-            title: 'Try it Out',
-          },
-        ]}
-      </Block>
-    ); */
-
-    /* const Description = () => (
-      <Block background="dark">
-        {[
-          {
-            content:
-              'This is another description of how this project is useful',
-            //image: `${baseUrl}img/docusaurus.svg`,
-            //imageAlign: 'right',
-            title: 'Description',
-          },
-        ]}
-      </Block>
-    ); */
 
     const LearnHow = () => (
       <Block background="light">
@@ -153,35 +93,6 @@ class Index extends React.Component {
       </Block>
     );
 
-    /* const Showcase = () => {
-      if ((siteConfig.users || []).length === 0) {
-        return null;
-      }
-
-      const showcase = siteConfig.users
-        .filter(user => user.pinned)
-        .map(user => (
-          <a href={user.infoLink} key={user.infoLink}>
-            <img src={user.image} alt={user.caption} title={user.caption} />
-          </a>
-        ));
-
-      const pageUrl = page => baseUrl + (language ? `${language}/` : '') + page;
-
-      return (
-        <div className="productShowcaseSection paddingBottom">
-          <h2>Who is Using This?</h2>
-          <p>This project is used by all these people</p>
-           <div className="logos">{showcase}</div> 
-          <div className="more-users">
-            <a className="button" href={pageUrl('users.html')}>
-              More {siteConfig.title} Users
-            </a>
-          </div>
-        </div>
-      );
-    }; */
-
     return (
       <div>
         <HomeSplash siteConfig={siteConfig} language={language} />
@@ -189,9 +100,6 @@ class Index extends React.Component {
           <Features />
           <FeatureCallout />
           <LearnHow /> 
-         {/*  <TryOut /> */}
-          {/* <Description /> */}
-         {/*  <Showcase /> */}
         </div>
       </div>
     );
