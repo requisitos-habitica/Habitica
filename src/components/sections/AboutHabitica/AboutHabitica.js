4e5a4105be50ae855296d3a89d0061390b520a41
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-04-07 15:39:42 -0300

    Style About section and add responsiveness to base font-size

--- src/components/sections/AboutHabitica/AboutHabitica.js
+++ src/components/sections/AboutHabitica/AboutHabitica.js
@@ -2,8 +2,8 @@ import React from 'react'
 import './AboutHabitica.scss'
 
 const AboutHabitica = () => (
-  <section className="about-habitica">
-    <h1 className="about-habitica__title">O que é o Habitica ?</h1>
+  <section className="content about-habitica">
+    <h1>O que é o Habitica ?</h1>
     <p className="about-habitica__text">
       Habitica é uma aplicação gamificada para ajudar seus usuários a criar ou melhorar hábitos na vida real.
       O app contém prêmios e punições para motivar o usuário e uma rede social para inspirá-lo.
