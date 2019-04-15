4871741b54e510d1986685329e85dbefe3dc5419
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-04-15 15:16:21 -0300

    Style component section and map component obj with Contributor UI component

--- src/components/UI/Contributor/Contributor.js
+++ src/components/UI/Contributor/Contributor.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Contributor = () => (
-  <div>
-    contr
-  </div>
+const Contributor = ({ github, name, img }) => (
+  <a href={ github } className="card">
+    <p>{ name }</p>
+    <img src={ img } alt="Contribuidor"/>
+  </a>
 )
 
 export default Contributor
--- src/components/sections/Contributors/Contributors.js
+++ src/components/sections/Contributors/Contributors.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import contributors from '../../../utils/contributors'
+import Contributor from '../../UI/Contributor/Contributor'
+import './Contributors.scss'
 
 const Contributors = () => (
-  contributors.map(contributor => <img src={ contributor.img } alt="Contribuidor"/>)
+  <section className="content contributors">
+    <h1>Contribuidores</h1>
+    { contributors.map(contributor => <Contributor { ...contributor }/>) }
+  </section>
 )
 
 export default Contributors
