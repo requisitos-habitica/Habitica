094cb4ed3c71e1456913b39c8cafd02d0a145180
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-04-14 23:38:17 -0300

    Export features data into /utils from a cleaner code and distribute its props to Feature UI component

--- src/components/sections/Features/Features.js
+++ src/components/sections/Features/Features.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Feature from '../../UI/Feature/Feature'
-import img from '../../../assets/inAppGroupAndStore.svg'
+import configFeatures from '../../../utils/configFeatures'
 import './Features.scss'
 
 const Features = () => (
   <section className="features">
-    <Feature title="Teste1" img={ img } description="Testandoooooo"/>
-    <Feature title="Teste2" img={ img } description="Testandoooooo"/>
+    <Feature { ...configFeatures.feat1 }/>
+    <Feature { ...configFeatures.feat2 }/>
   </section>
 )
 
---
+++ src/utils/configFeatures.js
@@ -0,0 +1,17 @@
+import featureImg1 from '../assets/inAppGroupAndStore.svg'
+import featureImg2 from '../assets/inAppHabits.svg'
+
+const configFeatures = {
+  feat1: {
+    title: 'Divirta-se organizando sua vida',
+    img: featureImg1,
+    description: 'Nunca foi tão legal organizar suas tarefas'
+  },
+  feat2: {
+    title: 'Evolua e desbrave o nosso Universo',
+    img: featureImg2,
+    description: 'Compre itens e crie grupos para lutar contra monstros'
+  }
+}
+
+export default configFeatures
