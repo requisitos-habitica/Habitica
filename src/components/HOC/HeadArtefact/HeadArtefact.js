ba868cbe1190edf18ceab04b2c8ed00269b96349
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-04-21 19:11:24 -0300

    CSS para a página de Modelagem e componente HeadArtefact inicial

---
+++ src/components/HOC/HeadArtefact/HeadArtefact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './HeadArtefact.scss'
+
+const HeadArtefact = () => {
+
+  return (
+    <h1>Head</h1>
+  )
+}
+
+export default HeadArtefact
--- src/components/docs/Modelagem/Lexico/Base.js
+++ src/components/docs/Modelagem/Lexico/Base.js
@@ -6,52 +6,35 @@ const Base = () => (
     <HeadArtefact versions={ null } rastreability={ null }
                   title="Artefato Nome" author="Quem fez"/>
     <Card>
-      <h2>Objetivo</h2>
+      <h2>Nome</h2>
       <p>Irure pariatur Lorem ea commodo tempor magna.</p>
     </Card>
 
     <Card>
-      <h2>Contexto</h2>
+      <h2>Sinônimos</h2>
       <ul>
-        <li>Esse sint enim minim <Link to="/Cenario08v2">Cenário08</Link> sit occaecat.</li>
-        <li>Adipisicing Lorem non mollit in nisi.</li>
+        <li>Esse sint <Link to="/Lexico04v2">Léxico04</Link></li>
       </ul>
     </Card>
 
     <Card>
-      <h2>Atores</h2>
+      <h2>Noção</h2>
       <ul>
-        <li><Link to="/Lexicos/Lexico04v2">Léxico04</Link> pariatur exercitation.</li>
+        <li>Reprehenderit sit adipisicing sunt tempor fugiat dolore eiusmod ea ad dolor.</li>
       </ul>
     </Card>
 
     <Card>
-      <h2>Recursos</h2>
+      <h2>Impacto</h2>
       <ul>
-        <li><Link to="/Lexicos/Lexico02v2">Léxico02</Link> aliqua</li>
-        <li>Fugiat aliqua</li>
+        <li><Link to="/Contextos/Contexto02v2">Léxico02</Link> aliqua</li>
+        <li>Aute incididunt pariatur ex dolore dolore culpa aliqua id ex minim velit.</li>
       </ul>
     </Card>
 
     <Card>
-      <h2>Episódios</h2>
-      <ul>
-        <li>Et veniam pariatur qui duis adipisicing.</li>
-      </ul>
-    </Card>
-
-    <Card>
-      <h2>Restrições</h2>
-      <ul>
-        <li>Elit esse in consequat ipsum.</li>
-      </ul>
-    </Card>
-
-    <Card>
-      <h2>Exceções</h2>
-      <ul>
-        <li>Elit esse in consequat ipsum.</li>
-      </ul>
+      <h2>Classificação</h2>
+      <p>Et veniam pariatur qui duis adipisicing.</p>
     </Card>
 
   </div>
