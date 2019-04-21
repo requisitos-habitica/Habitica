3d23897cb95b0502b1aba43b2de6f04944a75097
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-04-21 00:26:59 -0300

    Use Link component in the NavBar and style its hover

--- src/components/HOC/NavBar/NavBar.js
+++ src/components/HOC/NavBar/NavBar.js
@@ -2,18 +2,20 @@ import React from 'react'
 import './NavBar.scss'
 import logo from '../../../assets/habitica-logo.svg'
 
+import { Route, Link } from 'react-router-dom'
+
 const NavBar = props => (
   <>
     <nav className="nav">
-      <img src={ logo } alt="Habitica"/>
+      <Link to="/"><img src={ logo } alt="Habitica"/></Link>
       <ul>
-        <li>Disciplina</li>
-        <li>Pré-Rastreabilidade</li>
-        <li>Pós-Rastreabilidade</li>
-        <li>Elicitação</li>
-        <li>Modelagem</li>
-        <li>Análise</li>
-        <li>Sobre</li>
+        <Link to="/Disciplina">Disciplina</Link>
+        <Link to="/Pre-Rastreabilidade">Pré-Rastreabilidade</Link>
+        <Link to="/Pos-Rastreabilidade">Pós-Rastreabilidade</Link>
+        <Link to="/Elicitação">Elicitação</Link>
+        <Link to="/Modelagem">Modelagem</Link>
+        <Link to="/Analise">Análise</Link>
+        <Link to="/Sobre">Sobre</Link>
       </ul>
     </nav>
 
