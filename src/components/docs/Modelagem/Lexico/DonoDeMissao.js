49858bb9ed22b84ea6b51c0c305b00d12cbb2146
phe0 <pheofo@gmail.com>
2019-04-21 20:37:19 -0300

    Add template to all lexicos

---
+++ src/components/docs/Modelagem/Lexico/DonoDeMissao.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Base = () => (
+  <div className="content artefact">
+    <HeadArtefact versions={ null } rastreability={ null }
+                  title="Artefato Nome" author="Quem fez"/>
+    <Card>
+      <h2>Nome</h2>
+      <p>Irure pariatur Lorem ea commodo tempor magna.</p>
+    </Card>
+
+    <Card>
+      <h2>Sinônimos</h2>
+      <ul>
+        <li>Esse sint <Link to="/Lexico04v2">Léxico04</Link></li>
+      </ul>
+    </Card>
+
+    <Card>
+      <h2>Noção</h2>
+      <ul>
+        <li>Reprehenderit sit adipisicing sunt tempor fugiat dolore eiusmod ea ad dolor.</li>
+      </ul>
+    </Card>
+
+    <Card>
+      <h2>Impacto</h2>
+      <ul>
+        <li><Link to="/Contextos/Contexto02v2">Léxico02</Link> aliqua</li>
+        <li>Aute incididunt pariatur ex dolore dolore culpa aliqua id ex minim velit.</li>
+      </ul>
+    </Card>
+
+    <Card>
+      <h2>Classificação</h2>
+      <p>Et veniam pariatur qui duis adipisicing.</p>
+    </Card>
+
+  </div>
+)
+
+export default Base
