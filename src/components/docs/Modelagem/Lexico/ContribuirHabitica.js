e4876e60d07039cc8838fb043d92628df89881ee
saleh <saleh.nazih.dev@gmail.com>
2019-04-22 14:17:43 -0300

    Add Lexicos

---
+++ src/components/docs/Modelagem/Lexico/ContribuirHabitica.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const ContribuirHabitica = () => (
+  <div className="content artefact">
+    <HeadArtefact versions={ null } rastreability={ null }
+                  title="Contribuir para o Habitica" author="Saleh Kader"/>
+    <Card>
+      <h2>Nome</h2>
+      <p>Contribuir para o Habitica</p>
+    </Card>
+
+    <Card>
+      <h2>Sinônimos</h2>
+      <ul>
+        <li>-</li>
+      </ul>
+    </Card>
+
+    <Card>
+      <h2>Noção</h2>
+      <ul>
+        <li>
+            Usuário interessados no Habitica, podem ajudar no desenvolvimento geral do projeto.
+        </li>
+      </ul>
+    </Card>
+
+    <Card>
+      <h2>Impacto</h2>
+      <ul>
+        <li>
+            As contribuições para o Habitica, ajudam a melhorar a qualidade do projeto
+        </li> 
+      </ul>
+    </Card>
+
+    <Card>
+      <h2>Classificação</h2>
+      <p>Verbo</p>
+    </Card>
+
+  </div>
+)
+export default ContribuirHabitica
