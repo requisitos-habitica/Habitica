ff3cc69bc827000113fbc666cac3053101383446
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-04-22 00:45:41 -0300

    Atualiza o a estrutura com os lexicos para suportar suas cores e reduzir o map ao listá-las

--- src/utils/lexicos.js
+++ src/utils/lexicos.js
@@ -1,107 +1,131 @@
 const lexicos = {
-  "Fóruns": [
-    "Guerreiro",
-    "Bônus de atributo",
-    "Mago",
-    "Curador",
-    "Ladino",
-    "Força",
-    "Inteligência",
-    "Constituição",
-    "Percepção",
-    "Gema",
-    "Moeda",
-  ],
+  "Fóruns": {
+    color: "pink",
+    lexic: [
+      "Guerreiro",
+      "Bônus de atributo",
+      "Mago",
+      "Curador",
+      "Ladino",
+      "Força",
+      "Inteligência",
+      "Constituição",
+      "Percepção",
+      "Gema",
+      "Moeda",
+    ]
+  },
   
-  "Loja": [
-    "Cartões",
-    "Loja Sazonal",
-    "Viajantes do Tempo",
-    "Ampulheta Mística",
-    "Loja",
-  ],
+  "Loja": {
+    color: "aqua",
+    lexic: [
+      "Cartões",
+      "Loja Sazonal",
+      "Viajantes do Tempo",
+      "Ampulheta Mística",
+      "Loja",
+    ]
+  },
 
-  "Grupos": [
-    "Grupo",
-    "Chat",
-    "Membros"
-  ],
+  "Grupos": {
+    color: "SpringGreen",
+    lexic: [
+      "Grupo",
+      "Chat",
+      "Membros"
+    ]
+  },
 
-  "Gameficação": [
-    "Mana",
-    "Equipamento de Batalha",
-    "Fantasia",
-    "Guilda",
-    "Termos da Comunidade",
-    "Guilda Pública",
-    "Guilda Privada",
-    "Experiência",
-    "Chefão",
-    "Missão",
-    "Dar Dano",
-    "Convite de Missão",
-    "Tomo de Missão",
-    "Missão de Coleta",
-    "Missão de Mascote",
-    "Batalha de Chefão",
-    "Dono de missão",
-    "Alvo da Habilidade",
-  ],
+  "Gameficação": {
+    color: "Turquoise",
+    lexic: [
+      "Mana",
+      "Equipamento de Batalha",
+      "Fantasia",
+      "Guilda",
+      "Termos da Comunidade",
+      "Guilda Pública",
+      "Guilda Privada",
+      "Experiência",
+      "Chefão",
+      "Missão",
+      "Dar Dano",
+      "Convite de Missão",
+      "Tomo de Missão",
+      "Missão de Coleta",
+      "Missão de Mascote",
+      "Batalha de Chefão",
+      "Dono de missão",
+      "Alvo da Habilidade",
+    ]
+  },
 
-  "Guia de Contribuição": [
-    "Projeto de Código Aberto",
-    "Wiki",
-    "Contribuir para o Habitica",
-    "Ferreiros (Blacksmiths)",
-    "API do Habitica",
-    "Pixel Art",
-    "Traduzir o Habitica",
-    "Embaixadores",
-    "Escribas",
-    "Contadores de História",
-    "Desafiantes",
-    "Taverna",
-  ],
+  "Guia de Contribuição": {
+    color: "Orchid",
+    lexic: [
+      "Projeto de Código Aberto",
+      "Wiki",
+      "Contribuir para o Habitica",
+      "Ferreiros (Blacksmiths)",
+      "API do Habitica",
+      "Pixel Art",
+      "Traduzir o Habitica",
+      "Embaixadores",
+      "Escribas",
+      "Contadores de História",
+      "Desafiantes",
+      "Taverna",
+    ]
+  },
 
-  "Tarefas": [
-    "Tarefa",
-    "Tarefa Diária",
-    "Afazer",
-    "Hábito",
-    "Criar tarefa diária",
-    "Editar tarefa diária",
-    "Excluir tarefa diária",
-    "Completar tarefa diária",
-    "Resetar tarefa diária",
-    "Criar hábito",
-    "Editar hábito",
-    "Excluir hábito",
-    "Decrementar hábito",
-    "Incrementar hábito",
-    "Criar afazer",
-    "Editar afazer",
-    "Excluir o afazer",
-    "Concluir o afazer",
-    "Receber recompensa",
-  ],
+  "Tarefas": {
+    color: "Gold",
+    lexic: [
+      "Tarefa",
+      "Tarefa Diária",
+      "Afazer",
+      "Hábito",
+      "Criar tarefa diária",
+      "Editar tarefa diária",
+      "Excluir tarefa diária",
+      "Completar tarefa diária",
+      "Resetar tarefa diária",
+      "Criar hábito",
+      "Editar hábito",
+      "Excluir hábito",
+      "Decrementar hábito",
+      "Incrementar hábito",
+      "Criar afazer",
+      "Editar afazer",
+      "Excluir o afazer",
+      "Concluir o afazer",
+      "Receber recompensa",
+    ]
+  },
 
-  "Inventário": [
-    "Ovo de Pet",
-    "Chocar Ovo de Pet",
-    "Evoluir Pet",
-  ],
+  "Inventário": {
+    color: "LightCoral",
+    lexic: [
+      "Ovo de Pet",
+      "Chocar Ovo de Pet",
+      "Evoluir Pet",
+    ]
+  },
 
-  "Desafios": [
-    "Desafio",
-    "Categoria",
-    "Criar desafio",
-    "Prêmio",
-    "Se juntar ao desafio",
-    "Participante",
-    "Declarar vencedor do desafio",
-    "Deixar desafio",
-    "Adicionar tarefas de desafio",
-  ]
+  "Desafios": {
+    color: "DeepPink",
+    lexic: [
+      "Desafio",
+      "Categoria",
+      "Criar desafio",
+      "Prêmio",
+      "Se juntar ao desafio",
+      "Participante",
+      "Declarar vencedor do desafio",
+      "Deixar desafio",
+      "Adicionar tarefas de desafio",
+    ]
+  }
 }
 
 export default lexicos
