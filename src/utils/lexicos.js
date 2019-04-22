e1cbb9a7f1c7120f966dd850d4aacc9a1f19c39e
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-04-21 21:36:42 -0300

    Estrutura com os léxicos separados por categorias

---
+++ src/utils/lexicos.js
@@ -0,0 +1,105 @@
+const lexicos = {
+  "Fóruns": [
+    "Guerreiro",
+    "Bônus de atributo",
+    "Mago",
+    "Curador",
+    "Ladino",
+    "Força",
+    "Inteligência",
+    "Constituição",
+    "Percepção",
+    "Gema",
+    "Moeda",
+  ],
+  
+  "Loja": [
+    "Cartões",
+    "Loja Sazonal",
+    "Viajantes do Tempo",
+    "Ampulheta Mística",
+    "Loja",
+  ],
+
+  "Grupos": [
+    "Grupo",
+  ],
+
+  "Gameficação": [
+    "Mana",
+    "Equipamento de Batalha",
+    "Fantasia",
+    "Guilda",
+    "Termos da Comunidade",
+    "Guilda Pública",
+    "Guilda Privada",
+    "Experiência",
+    "Chefão",
+    "Missão",
+    "Dar Dano",
+    "Convite de Missão",
+    "Tomo de Missão",
+    "Missão de Coleta",
+    "Missão de Mascote",
+    "Batalha de Chefão",
+    "Dono de missão",
+    "Alvo da Habilidade",
+  ],
+
+  "Guia de Contribuição": [
+    "Projeto de Código Aberto",
+    "Wiki",
+    "Contribuir para o Habitica",
+    "Ferreiros (Blacksmiths)",
+    "API do Habitica",
+    "Pixel Art",
+    "Traduzir o Habitica",
+    "Embaixadores",
+    "Escribas",
+    "Contadores de História",
+    "Desafiantes",
+    "Taverna",
+  ],
+
+  "Tarefas": [
+    "Tarefa",
+    "Tarefa Diária",
+    "Afazer",
+    "Hábito",
+    "Criar tarefa diária",
+    "Editar tarefa diária",
+    "Excluir tarefa diária",
+    "Completar tarefa diária",
+    "Resetar tarefa diária",
+    "Criar hábito",
+    "Editar hábito",
+    "Excluir hábito",
+    "Decrementar hábito",
+    "Incrementar hábito",
+    "Criar afazer",
+    "Editar afazer",
+    "Excluir o afazer",
+    "Concluir o afazer",
+    "Receber recompensa",
+  ],
+
+  "Inventário": [
+    "Ovo de Pet",
+    "Chocar Ovo de Pet",
+    "Evoluir Pet",
+  ],
+
+  "Desafios": [
+    "Desafio",
+    "Categoria",
+    "Criar desafio",
+    "Prêmio",
+    "Se juntar ao desafio",
+    "Participante",
+    "Declarar vencedor do desafio",
+    "Deixar desafio",
+    "Adicionar tarefas de desafio",
+  ]
+}
+
+export default lexicos
