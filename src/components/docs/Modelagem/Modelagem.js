a9acda775abb26385d322c485e3ae848865a0a90
Rogério Júnior <junior.rogerio8@gmail.com>
2019-05-08 15:56:19 -0300

    Fixed routes from Modelagem component

--- src/components/docs/Modelagem/Modelagem.js
+++ src/components/docs/Modelagem/Modelagem.js
@@ -8,14 +8,14 @@ const Modelagem = (props) => (
   <div className="content artefact">
     <h1>Modelagem</h1>
     <div className="modelagem">
-      <Link target="_blank" to={ `${props.match.url}/Lexicos` } style={{ textDecoration: 'none' }}>
+      <Link target="_blank" to="Modelagem/Lexicos" style={{ textDecoration: 'none' }}>
         <div className="cards">
           <img src={ dictionary } alt="Léxico"/>
           <h2>Léxicos</h2>
         </div>
       </Link>
 
-      <Link target="_blank" to={ `${props.match.url}/Cenarios` } style={{ textDecoration: 'none' }}>
+      <Link target="_blank" to="Modelagem/Cenarios" style={{ textDecoration: 'none' }}>
         <div className="cards">
           <img src={ route } alt="Cenário"/>
           <h2>Cenários</h2> 
