5962d15b62b42651e85732b378188ebf69ddf0be
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-05-26 21:50:25 -0300

    Opções para Pre-Rastreabilidade com o componente NCardsLayout e adicionando um favicon e titulo da pagina

--- src/components/docs/PreRastreabilidade/PreRastreabilidade.js
+++ src/components/docs/PreRastreabilidade/PreRastreabilidade.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import NCardsLayout from '../../UI/NCardsLayout/NCardsLayout'
+import richpictureIcon from '../../../assets/rich-picture.svg'
+import argumentacaoIcon from '../../../assets/argumentacao.svg'
+
+const items = [
+  {
+    link: '/Argumentacoes',
+    img: argumentacaoIcon,
+    title: 'Argumentações'
+  },
+  {
+    link: '/RichPictures',
+    img: richpictureIcon,
+    title: 'Rich Pictures'
+  }
+]
 
 const PreRastreabilidade = () => (
   <div className="content artefact">
     <h1>Pré-Rastreabilidade</h1>
-
+    <NCardsLayout items={ items }/>
   </div>
 )
 
