391e471a6b86be2cef84cda3938ceef5f30d60d0
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-05-26 21:27:26 -0300

    Componente para renderizar cards para outros tópicos

---
+++ src/components/UI/NCardsLayout/NCardsLayout.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './NCardsLayout.scss'
+
+const NCardsLayout = props => (
+  <div className="n-cards-layout">
+    {
+      props.items.map(item => (
+        <Link to={ item.link } style={{ textDecoration: 'none' }}>
+          <div className="cards">
+            <img src={ item.img } alt="Léxico"/>
+            <h2>{ item.title }</h2>
+          </div>
+        </Link>
+      ))
+    }
+  </div>
+)
+
+export default NCardsLayout
