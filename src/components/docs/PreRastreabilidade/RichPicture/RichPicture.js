0ebbb4f750c73f183a7d2fed7b15c8ed5f61c38f
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-06-02 20:22:51 -0300

    Travessia na estrutura que definida para renderizar as RPs dinamicamente

--- src/components/docs/PreRastreabilidade/RichPicture/RichPicture.js
+++ src/components/docs/PreRastreabilidade/RichPicture/RichPicture.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import richpictures from '../../../../utils/richpictures'
 
+import './RichPicture.scss'
+
 const RichPicture = () => {
 
   console.log(richpictures)
@@ -8,6 +10,22 @@ const RichPicture = () => {
   return (
     <div className="content artefact">
       <h1>Rich Pictures</h1>
+      
+        { Object.keys(richpictures).map(group => (
+            <>
+              <h2>{ group }</h2>
+              <div className="cards-grid">
+                { Object.keys(richpictures[group]).map(contributor => (
+                  <>
+                  <p>{contributor}</p>
+                  {richpictures[group][contributor].map((rp, i) => <img src={richpictures[group][contributor][i]} style={{width:"10rem", height:"10rem"}}/>)}
+                  </>
+                )) }
+              </div>
+            </>
+          )
+        ) }
+
     </div>
   )
 }
