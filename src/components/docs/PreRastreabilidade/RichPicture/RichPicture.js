765a39b395649776b90b9ed8b846f2c505f47924
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-06-02 21:01:23 -0300

    Melhoria na travessia e adição do CSS para os RPs

--- src/components/docs/PreRastreabilidade/RichPicture/RichPicture.js
+++ src/components/docs/PreRastreabilidade/RichPicture/RichPicture.js
@@ -12,17 +12,21 @@ const RichPicture = () => {
       <h1>Rich Pictures</h1>
       
         { Object.keys(richpictures).map(group => (
-            <>
+            <div className="cards-grid-container" key={ group }>
               <h2>{ group }</h2>
               <div className="cards-grid">
                 { Object.keys(richpictures[group]).map(contributor => (
-                  <>
-                  <p>{contributor}</p>
-                  {richpictures[group][contributor].map((rp, i) => <img src={richpictures[group][contributor][i]} style={{width:"10rem", height:"10rem"}}/>)}
-                  </>
+                  <React.Fragment key={contributor + Math.random()}>
+                    {richpictures[group][contributor].map((rp, i) => (
+                      <div>
+                        <p>{ `RP${ contributor.split(' ')[0] }V${ i + 1 }` }</p>
+                        <img src={ richpictures[group][contributor][i] } key={ contributor + i }/>)
+                      </div>
+                    ))}
+                  </React.Fragment>
                 )) }
               </div>
-            </>
+            </div>
           )
         ) }
 
