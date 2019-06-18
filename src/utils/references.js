b8e29cb43556cac908a22f7f496fe084fd85f420
EsioFreitas <esio.gustavo@gmail.com>
2019-06-17 21:36:22 -0300

    add modelagem references

--- src/utils/references.js
+++ src/utils/references.js
@@ -4,8 +4,25 @@ const preRastrabilidade = [
     'Analysis of Multi-Party Agreement in Requirements Validation. Ivan J. Jureta, John Mylopoulos e Stéphane Faulkner.    '
 ]
 const prosRastrabilidade = []
-const elicitacao = []
-const modelagem = []
+const elicitacao = [
+    'Use Case Spotlight. Acessado em 06/05/2018. Em <https://habitica.wordpress.com/category/use-case-spotlight/>    ',
+    'First Things First: Prioritizing Requirements - Karl E. Wiegers',
+    'Techniques for Requirements Elicitation - Goguen and Linde',
+    'Quality Function Deployment (QFD) and House of Quality - MIT OpenCourseWare - YouTube. Em <https://www.youtube.com/watch?v=u9bvzE5Qhjk>',
+    "QFD: an interactive algorithm for the prioritization of product's technical design characteristics - Fraceschini & Rossetto"
+]
+const modelagem = [
+    "A wikia brasileira do Habitica. Acessado em 19/04/2019. Em <https://habitica.fandom.com/pt-br/wiki/>",
+    "The courtyard. Acessado em 19/04/2019. Em <https://habitica.fandom.com/wiki/Special:Forum>",
+    "Contributing to Habitica. Acessado em 19/04/2019. Em <https://habitica.fandom.com/wiki/Contributing_to_Habitica>",
+    "Habitica Pixel Art - Trello. Em <https://trello.com/b/vwuE9fbO/habitica-pixel-art>",
+    "Privacy Policy. Acessado em 28/04/2019. Em <https://habitica.com/static/privacy>",
+    "Staff. Acessado em 19/04/2019. Em <https://habitica.fandom.com/wiki/Staff>",
+    "Yu-kai Chou: Gamification & Behavioral Design",
+    "Jakob Nielsen: 10 Heuristics for User Interface Design",
+    "iStar Wiki. Acessado em 27/05/2019. Em <http://istarwiki.org/tiki-view_articles.php>",
+    "iStar Quick Guide - Trello. Em <http://istarwiki.org/tiki-index.php?page=iStarQuickGuide>"
+]
 const analise  = []
 
 export default {
