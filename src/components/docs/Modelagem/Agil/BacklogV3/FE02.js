8b4f4ffc3baa2858b29541a36185c58259c740f9
Lucas Dutra <ldutra98@gmail.com>
2019-06-22 19:08:57 -0300

    Add Product Backlog v3

---
+++ src/components/docs/Modelagem/Agil/BacklogV3/FE02.js
@@ -0,0 +1,19 @@
+const FE02 = [
+    {
+        id: 'US17',
+        description: 'Eu, como usuário do sistema, desejo deletar uma etiqueta para não bagunçar minha organização'
+    },
+    {
+        id: 'US18',
+        description: 'Eu, como usuário do sistema, desejo atualizar uma etiqueta para arrumar dados incorretos ou desatualizados'
+    },
+    {
+        id: 'US19',
+        description: 'Eu, como usuário do sistema, desejo reorganizar a ordem das etiquetas para eu poder organizar da forma que eu quiser'
+    },
+    {
+        id: 'US20',
+        description: 'Eu, como usuário do sistema, desejo ver atividades completadas para eu saber o que ja foi finalizado'
+    },
+]
+export default FE02
