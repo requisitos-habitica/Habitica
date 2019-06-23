09d77b3b84760f9ee2a14da973bf0323efc76d4b
EsioFreitas <esio.gustavo@gmail.com>
2019-06-23 12:37:12 -0300

    Add sobre imgs

--- src/components/docs/sobre/Sobre.js
+++ src/components/docs/sobre/Sobre.js
@@ -3,7 +3,30 @@ import './Sobre.scss';
 
 const Sobre = () => (
     <div className="sobre-page">
-
+        <h1>Metodologia</h1>
+        <div>
+            <div>
+                Drive
+            </div>
+            <div>
+                Organização do time
+            </div>
+            <div>
+                Uso de issues
+            </div>
+            <div>
+                Uso de labels
+            </div>
+            <div>
+                cometarios
+            </div>
+            <div>
+                commit
+            </div>
+            <div>
+                telegram
+            </div>
+        </div>
     </div>
 );
 
