668b1cb1988752da264c4eb8d449523377794177
Rogério Júnior <junior.rogerio8@gmail.com>
2019-06-24 04:51:10 -0300

    Upddating repeated icons

--- src/components/docs/Analise/Validacao.js
+++ src/components/docs/Analise/Validacao.js
@@ -2,29 +2,30 @@ import React from 'react'
 import NCardsLayout from '../../UI/NCardsLayout/NCardsLayout'
 import dictionary from '../../../assets/dictionary.svg'
 import route from '../../../assets/destination.svg'
-import checklist from '../../../assets/checklist.svg'
-import correct from '../../../assets/correct.svg'
+import prototype from '../../../assets/prototype.svg'
+import email from '../../../assets/email.svg'
+import gryphon from '../../../assets/gryphon.svg'
 import Referencias from '../../UI/reference/reference'
 
 const items = [
   {
     link: '/Prototipo',
-    img: checklist,
+    img: prototype,
     title: 'Protótipo'
   },
   {
     link: '/emailMatteo',
-    img: correct,
+    img: email,
     title: 'Validação Informal Matteo'
   },
   {
     link: '/emailAlys',
-    img: correct,
+    img: email,
     title: 'Validação Informal Alys'
   },
   {
     link: '/WikiOficial',
-    img: correct,
+    img: gryphon,
     title: 'Wiki Oficial'
   }
 ]
